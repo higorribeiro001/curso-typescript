ff33f79317f69ad772c7b9aeafaff04bdc620f64
Higor Araujo <higorribeiroaraujo007@gmail.com>
2025-08-07 12:57:23 -0300

    feat: webpack para front e para back separados

---
+++ webpack.config copy.cjs
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/A0018-webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: { configFile: ['src/////.{ts,tsx}', '!src/skip.ts'] }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map'
+};
