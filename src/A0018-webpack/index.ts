49c30f8c4f423c57c5295eb8976573e876da0a6b
Higor <higorribeiroaraujo007@gmail.com>
2025-08-07 00:12:57 -0300

    feat: webpack

---
+++ src/A0018-webpack/index.ts
@@ -0,0 +1,10 @@
+// npm i ts-loader webpack-cli --save-dev --force
+
+// doc: https://webpack.js.org/guides/typescript/
+
+// rodar: npx webpack
+
+// para ficar sempre assistindo: npx webpack -w
+
+import funcao from './mod';
+funcao();
