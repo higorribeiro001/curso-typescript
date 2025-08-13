18fd43059a4f2c984660d5f0af6175f7a2357ec1
Higor Araujo <higorribeiroaraujo007@gmail.com>
2025-08-13 13:24:44 -0300

    feat: classe, metodos e atributos abstratos

---
+++ src/A0029-abstract/A0029-abstract.ts
@@ -0,0 +1,42 @@
+export class Personagem {
+  protected abstract emoji: string;
+
+  constructor(
+    protected nome: string,
+    protected ataque: number,
+    protected vida: number
+  ) {}
+
+  atacar(personagem: Personagem): void {
+    // console.log(`${this.nome} está atacando...`);
+    this.bordao();
+    personagem.perderVida(this.ataque);
+  }
+
+  perderVida(forcaAtaque: number): void {
+    this.vida -= forcaAtaque;
+    console.log(`${this.emoji} - ${this.nome} agora tem ${this.vida} de vida...`);
+  }
+
+  abstract bordao(): void ;
+}
+
+export class Guerreira extends Personagem {
+  protected emoji = '\u{1F9DD}';
+
+  bordao(): void {
+    console.log(this.emoji + ' GUERREIRAAAAA AOOOO ATAAQUEEEE!!!!!');
+  }
+}
+export class Monstro extends Personagem {
+  protected emoji = '\u{1F9DF}';
+
+  bordao(): void {
+    console.log(this.emoji + ' MONSTROOOOOOOOOOOOOOOOO!!!!!');
+  }
+}
+
+const guerreira = new Guerreira('Guerreira', 100, 1000);
+const monstro = new Monstro('Monstro', 87, 1000);
+
+guerreira.atacar(monstro);
